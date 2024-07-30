18f6a4071412014cbb454671eb357f0a2b82190b
Miriam Martin Casas <miriam.martin.casas@gmail.com>
2024-07-30 12:24:11 +0200

    Inicio proyecto y consulta API

---
+++ README.md
@@ -0,0 +1,24 @@
+# Sprint 4 IT Academy | TypeScript & API
+
+## Introducción
+
+App web que muestra chistes.
+
+<br>
+
+## Requirements
+
+
+1. Clonar el repositorio:
+```bash
+$ git clone https://github.com/mmartincasas/Sprint-4
+```
+
+2. Desvincula tu repositorio del repositorio del repositorio actual
+```bash
+$ git remote rm origin
+```
+
+3. Crea tu propio repositorio en Github y vinculalo a él
+```bash
+$ git remote add origin <your repo name!>
---
+++ main.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const jokesDiv = document.getElementById('jokesDiv')
+
+
+// Función para obtener un chiste aleatorio
+async function obtenerChisteAleatorio() {
+    try {
+        // Realiza la solicitud a la API
+        const response = await fetch('https://icanhazdadjoke.com/', {
+            headers: {
+                'Accept': 'application/json',
+            }
+        });
+
+        // Verifica si la respuesta es exitosa
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+
+        // Analiza la respuesta como JSON
+        const data = await response.json();
+
+        // Muestra el chiste en la consola
+        console.log(data.joke);
+        
+        jokesDiv.innerHTML = data.joke;
+
+
+    } catch (error) {
+        console.error('Error al obtener el chiste:', error);
+    }
+}
+
+// Llama a la función para obtener un chiste aleatorio
+obtenerChisteAleatorio();
