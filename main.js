21485e135e3bbb2345e49da1127d302563afb471
Miriam Martin Casas <miriam.martin.casas@gmail.com>
2024-08-02 18:32:02 +0200

    Api de tiempo en Barcelona

--- main.js
+++ main.js
@@ -9,30 +9,47 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const jokesDiv = document.getElementById('jokesDiv');
-// Función para obtener un chiste aleatorio
+const weatherDescription = document.getElementById('weatherDescription');
+const weatherIcon = document.getElementById('weatherIcon');
+const weatherTemp = document.getElementById('weatherTemp');
+const weatherHumidity = document.getElementById('weatherHumidity');
+function getWeather() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch(`https://api.openweathermap.org/data/2.5/weather?q=Barcelona&units=metric&appid=96cf2de381520089b251bc30cc3baf52`);
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
+            const data = yield response.json();
+            console.log(data);
+            weatherDescription.innerHTML = `${data.weather[0].description.toUpperCase()}`;
+            weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}">`;
+            weatherTemp.innerHTML = `${data.main.temp} °C`;
+            weatherHumidity.innerHTML = `${data.main.humidity} %`;
+        }
+        catch (error) {
+            console.error('Error getting Weather', error);
+        }
+    });
+}
 function getRandomJoke() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            // Realiza la solicitud a la API
             const response = yield fetch('https://icanhazdadjoke.com/', {
                 headers: {
                     'Accept': 'application/json',
                 }
             });
-            // Verifica si la respuesta es exitosa
             if (!response.ok) {
                 throw new Error(`Error: ${response.status}`);
             }
-            // Analiza la respuesta como JSON
             const data = yield response.json();
-            // Muestra el chiste en la consola
-            console.log(data.joke);
             jokesDiv.innerHTML = data.joke;
         }
         catch (error) {
-            console.error('Error al obtener el chiste:', error);
+            console.error('Error getting joke', error);
         }
     });
 }
-// Llama a la función para obtener un chiste aleatorio
 getRandomJoke();
+getWeather();
--- main.ts
+++ main.ts
@@ -1,34 +1,52 @@
 'use strict'
 
 const jokesDiv = document.getElementById('jokesDiv')!;
+const weatherDescription = document.getElementById('weatherDescription')!;
+const weatherIcon = document.getElementById('weatherIcon')!;
+const weatherTemp = document.getElementById('weatherTemp')!;
+const weatherHumidity = document.getElementById('weatherHumidity')!;
 
-// Función para obtener un chiste aleatorio
+async function getWeather() {
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Barcelona&units=metric&appid=96cf2de381520089b251bc30cc3baf52`)
+
+        if (!response.ok){
+            throw new Error (`Error: ${response.status}`)
+        }
+
+        const data = await response.json();
+        console.log(data)
+
+        weatherDescription.innerHTML = `${data.weather[0].description.toUpperCase()}`;
+        weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}">`;
+        weatherTemp.innerHTML = `${data.main.temp} °C`;
+        weatherHumidity.innerHTML = `${data.main.humidity} %`;
+
+    }catch (error){
+        console.error('Error getting Weather', error)
+    }
+}
+
 async function getRandomJoke() {
     try {
-        // Realiza la solicitud a la API
         const response = await fetch('https://icanhazdadjoke.com/', {
             headers: {
                 'Accept': 'application/json',
             }
         });
 
-        // Verifica si la respuesta es exitosa
         if (!response.ok) {
             throw new Error(`Error: ${response.status}`);
         }
 
-        // Analiza la respuesta como JSON
         const data = await response.json();
-
-        // Muestra el chiste en la consola
-        console.log(data.joke);
-        
         jokesDiv.innerHTML = data.joke;
 
     } catch (error) {
-        console.error('Error al obtener el chiste:', error);
+        console.error('Error getting joke', error);
     }
 }
 
-// Llama a la función para obtener un chiste aleatorio
 getRandomJoke();
+getWeather();
+
